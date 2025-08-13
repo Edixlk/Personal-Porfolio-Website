8a867d33256b9c52dac52d7cf423049dc5ea11d2
edixlk <114295187+Edixlk@users.noreply.github.com>
2025-08-13 11:43:47 -0400

    dev branch update 8.13.25

--- main.js
+++ main.js
@@ -1,51 +1,9 @@
-// ***FOR main BRANCH***
+// ***FOR DEV BRANCH***
 
+console.log("working...");
+console.log("https://github.com/Edixlk/");
 
-// FOR MOBILE NAVBAR
+// FOR MOBILE NAVBAR (*****REMOVED*****)
 
-let contactHovr = document.getElementById("contact-flt-btn");
+// FOR CONTACT FORM(email.js) (***REMOVED UNTIL WHENEVER****)
 
-let MenuBtn = document.getElementById("Hamburg-menu");
-
-function Open() {
-  let NavigLinks = document.getElementById("navlinks");
-
-  contactHovr.style.display = "none";
-
-  if (NavigLinks.style.display == "flex") {
-    NavigLinks.style.display = "none";
-    contactHovr.style.display = "block";
-  } else {
-    NavigLinks.style.display = "flex";
-    contactHovr.style.display = "none";
-  }
-}
-
-// FOR CONTACT FORM(email.js)
-
-let PopupMsg = document.getElementById("msgpopup");
-
-let Successtxt = document.getElementById("Sucesstxt");
-
-let Errortxt = document.getElementById("Errtxt");
-
-function Sendmail() {
-  const templateParams = {
-    name: document.querySelector("#name").value,
-    name: document.querySelector("#emailadr").value,
-     
-  };
-
-  emailjs.send("service_eojdgfk", "template_xoc6n7p", templateParams).then(
-    (response) => {
-      console.log("SUCCESS!", response.status, response.text);
-      Successtxt.innerHTML = "Your Email was sent!"; // Sucess Message
-      Errortxt.style.display = "none";
-    },
-    (error) => {
-      console.log("FAILED...", error);
-      Errortxt.innerHTML = "Sorry, Please try again or complete the form."; // Error Message
-      Successtxt.style.display = "none";
-    }
-  );
-}
