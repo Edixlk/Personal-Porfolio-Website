d125328934c02cf7eccb397d0ded6c278618eb6d
edixlk <114295187+Edixlk@users.noreply.github.com>
2025-12-10 11:09:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let MenuBtn = document.getElementById("Hamburg-menu");
 let NavigLinks = document.getElementById("navlinks");
 
 function Open() {
-  if (NavigLinks.style.display == "none") {
+  if (NavigLinks.style.display = "none") {
     NavigLinks.style.display = "flex";
   } else {
     NavigLinks.style.display = "none";
